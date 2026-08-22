85f8a1a49aef174cfaad67d6f8868140c541b2f8
swapnaamara <swapnaamara59@gmail.com>
2026-08-22 21:37:59 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,11 +1,12 @@
 n = int(input("Enter a number: ")) 
 original = n
 r = 0
-while n > 0:
+while n > 0
     r = r*10 + n%10
     n = n//10
 
 if original == r:
     print("Palindrome") 
+
 else:
     print("Not Palindrome")
