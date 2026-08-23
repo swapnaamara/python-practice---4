919930d6590498d8ff83b58797e453475e714885
swapnaamara <swapnaamara59@gmail.com>
2026-08-23 14:31:17 +0530

    Add files via upload

---
+++ Largest of three numbers.py
@@ -0,0 +1,10 @@
+a = int(input("first number: ")) 
+b = int(input("second number: ")) 
+c = int(input("third number: ")) 
+
+if a > b and a > c:
+    print(a)
+elif b > c:
+    print(b)
+else:
+    print(c) 
