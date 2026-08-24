6bf55d6e6c7293eb71f483757402c91e978038ca
swapnaamara <swapnaamara59@gmail.com>
2026-08-24 20:00:55 +0530

    Add files via upload

---
+++ LCM of two numbers.py
@@ -0,0 +1,13 @@
+a = int(input("first number: ")) 
+b = int(input("second number: ")) 
+
+x = a
+y = b
+while y!= 0:
+    temp = y
+    y = x % y
+    x = temp
+
+gcd = x
+lcm = (a * b) // gcd
+print(lcm) 
