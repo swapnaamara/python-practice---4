3972b8bd01554af6b07d878c58a691ae9ea22cae
swapnaamara <swapnaamara59@gmail.com>
2026-08-24 20:06:18 +0530

    Add files via upload

---
+++ Count vowels and consonents.py
@@ -0,0 +1,13 @@
+s = input("Enter a string: ").lower() 
+vowels = 0
+consonants = 0
+
+for ch in s:
+    if ch >= 'a' and ch <= 'z': 
+        if ch in "aeiou":
+            vowels += 1
+        else:
+            consonants += 1
+
+print("Vowels:", vowels) 
+print("Consonants:", consonants) 
